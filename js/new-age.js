ffc7e1d766e7db9b403848156d5dbba5a9e819c0
Barbey Ng <barbeyng@er-10-129-153-203.wireless.umn.edu>
2018-05-03 19:52:26 -0500

    cleaner

--- js/new-age.js
+++ js/new-age.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +16,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -27,7 +27,7 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
@@ -40,3 +40,85 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+//Calorie Tracker
+$('#submit-button').click(searchFood);
+function searchFood(event) {
+  event.preventDefault();
+  var userInput = $('#foodInput').val().trim();
+
+  console.log(userInput);
+  var nutritionix = "https://nutritionix-api.p.mashape.com/v1_1/search/" + userInput + "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat"
+  $.ajax({
+    url: nutritionix,
+    headers: {
+      "X-Mashape-Key": "3XWHBPKtGMmshHNn3AQ1pj1tDxMCp1YlCKrjsnEh2iA8rmMQJ5",
+      "Accept": "application/json",
+    },
+    method: "GET"
+  }).done(function (response) {
+    console.log(response);
+    console.log(response.hits[0].fields.nf_calories);
+    $("#table > tbody").append("<tr><td>" + userInput + "</td><td>" + response.hits[0].fields.nf_calories + "</td><td>" + response.hits[0].fields.item_name + "</td></tr>");
+  });
+};
+
+//Google Maps API
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -33.8688, lng: 151.2195 },
+    zoom: 13,
+    mapTypeId: 'roadmap'
+  });
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function () {
+    searchBox.setBounds(map.getBounds());
+  });
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function () {
+    var places = searchBox.getPlaces();
+    if (places.length == 0) {
+      return;
+    }
+    // Clear out the old markers.
+    markers.forEach(function (marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function (place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
